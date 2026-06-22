61f0a97102aee278ae38d16c748e7a3d9b1d8fa7
s1y4x1 <13051081932@139.com>
2026-06-22 19:24:47 +0800

    更新配置文件以支持动态输入tid和STUDY_SESS，优化并发限制提示，添加考试功能

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import requests
+termId=int(
+1476491494
+)#填写课程url中的tid
 #前往浏览器登录后获取对应Cookie
 STUDY_SESS='''
-#在此处粘贴STUDY_SESS Cookie
+"2rP32wlCWutbSBzL3WvXX7HlqSsBJImjkVZyIHRdE9SbrBjiRLwazwuuLtLCoBccw6131oB2gWL29zCLd3+PEtXAZaEFfeXA6bQedO+spt5i5+7GeXDEgZ7Ql8UEv8vXSiGVJbrca51DO2Z/0kJOeBoOR4kE435+Vl4R3cNN3UgLhur2Nm2wEb9HcEikV+3FTI8+lZKyHhiycNQo+g+/oA=="
 '''.strip()
 NTESSTUDYSI='0'#非必需
 url_down = "https://www.icourse163.org/web/j/mocQuizRpcBean.getOpenHomeworkPaperDto.rpc"
@@ -20,5 +23,6 @@ params={'csrfKey': NTESSTUDYSI}
 post=lambda url,data:requests.post(url,params=params,headers=headers,cookies=cookies,json=data).json()
 if __name__=='__main__':
     l=open('config.py', 'r', encoding='utf-8').readlines()
-    l[3]=input('STUDY_SESS: ')+'\n'
+    l[2]=(input('tid: ')or l[2][:-1])+'\n'
+    l[6]=(input('STUDY_SESS: ')or l[6][:-1])+'\n'
     with open('config.py','w',encoding='utf-8')as f:f.writelines(l)
--- 单元作业.py
+++ 单元作业.py
@@ -1,7 +1,7 @@
 from config import *
 get_homework_result=lambda tid:post(url_down,{"tid": tid,"withStdAnswerAndAnalyse": False})['result']
 def homework(tid):
-    while not(result:=get_homework_result(tid)):print('可能遇到并发限制，正在重试……',end='\r')
+    while not(result:=get_homework_result(tid)):print('遇并发限制，正在重试……',end='\r')
     answers=[]
     for answer in result["subjectiveQList"]:
         answers+=[{'qid':answer['id'],'type':answer['type'],'content':{
@@ -16,7 +16,7 @@ def has_empty(tid):
     print('检查作业提交情况……',end='\r')
     while not(result:=get_homework_result(tid)):print('遇并发限制，正在重试……',end='\r')
     if not result['answers']:
-        print(f'{result=}')
+        print(f'[DEBUG]{result=}')
         return True
     for answer in result['answers']:
         content=answer['content']
